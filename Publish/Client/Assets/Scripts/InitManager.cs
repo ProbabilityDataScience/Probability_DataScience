b7c9c9a48537ba82dd65540ad04904ee5afbff5d
Lim HJ <max804@naver.com>
2016-11-15 22:57:47 +0900

    InitScene 추가 / Facebook SDK 설치

---
+++ Publish/Client/Assets/Scripts/InitManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook.Unity;
+
+public class InitManager : MonoBehaviour {
+
+	void Awake()
+	{
+		// 페북 SDK 초기화
+		FB.Init(InitFB_Complete);
+
+	}
+
+	// 페북 초기화 성공
+	private void InitFB_Complete()
+	{
+		FB.ActivateApp();
+		// 작성중...
+	}
+}
